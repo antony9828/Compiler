38d7f5b6b9beac4291d7c4fc8e2a78209e8687a1
Anton <anton.pushkin2@gmail.com>
2019-04-30 00:11:15 +0300

    Initial commit

---
+++ Compiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Compiler
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
